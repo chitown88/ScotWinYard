9d59d035a38ba6063648606d240a9fa5299c63ce
Schvach <js185529@ncr.com>
2019-10-14 14:06:32 +0100

    Update

--- js/script2.js
+++ js/script2.js
@@ -1,20 +1,13 @@
 
 
-
 $(document).ready(function() {
-var div_ele = document.querySelectorAll('.glider');
-
-new Glider(div_ele[1], {
-		  slidesToShow: 5,
-		  slidesToScroll: 1,
-		  rewind: true,
-		  draggable: true,
-		  dots: '.dots',
-		  arrows: {
-	      prev: '.glider-prev',
-		  next: '.glider-next'
-		  }
-	
+$('.week-list').slick({
+  dots: true,
+  infinite: true,
+  speed: 500,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  cssEase: 'linear'
 });
 });
 
